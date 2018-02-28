9de13eceba3df4cfbbb65a5577bdc2a8e7474f14
MimiMicek <makaveli_girl1996@hotmail.com>
2018-02-28 12:33:14 +0100

    Final version

--- myScript.js
+++ myScript.js
@@ -1,19 +1,19 @@
 
 function jsonToUI() {
     var developerList = JSON.parse(this.responseText);
-    for(var i=0;i<developerList.length;i++) {
-        document.getElementById("demo0").innerHTML+=developerList[i].Id+"<br><br>";
-        document.getElementById("demo").innerHTML+=developerList[i].Name+"<br><br>";
-        document.getElementById("demo2").innerHTML+=developerList[i].Email+"<br><br>";
+    for (var i = 0; i < developerList.length; i++) {
+        document.getElementById("demo0").innerHTML += developerList[i].Id + "<br><br>";
+        document.getElementById("demo").innerHTML += developerList[i].Name + "<br><br>";
+        document.getElementById("demo2").innerHTML += developerList[i].Email + "<br><br>";
         //console.log(developerList[i].Name);
     }
 }
 
 var oReq = new XMLHttpRequest();
 
-oReq.addEventListener("load",jsonToUI);
+oReq.addEventListener("load", jsonToUI);
 
-oReq.open("GET","http://dm.sof60.dk:84/api/Developer");
+oReq.open("GET", "http://dm.sof60.dk:84/api/Developer");
 
 oReq.send();
 
